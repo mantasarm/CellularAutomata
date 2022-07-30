38c53bb4f76f9260aef5b2519f8eadedbc55d528
mantasarm <34283640+mantasarm@users.noreply.github.com>
2022-07-30 14:58:31 +0300

    Delete src directory

--- src/engine.rs
+++
@@ -1,5 +0,0 @@
-pub mod engine;
-pub mod camera;
-pub mod shader;
-pub mod shape_renderer;
-pub mod triangle;
--- src/engine/fragment.glsl
+++
@@ -1,7 +0,0 @@
-#version 140
-
-in vec4 fColor;
-
-void main() {
-    gl_FragColor = fColor;
-}
